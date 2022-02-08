1e548abde12596a7a8d9d29dae60696da38f40c9
Sergey Ukustov <sergey@ukstv.me>
2022-02-08 18:52:14 +0300

    wip

--- packages/core/src/store/state-store.ts
+++ packages/core/src/store/state-store.ts
@@ -1,6 +1,9 @@
 import type { StreamState, StreamStateHolder } from '@ceramicnetwork/common'
 import type { StreamID } from '@ceramicnetwork/streamid'
 
+/**
+ * Generic state store interface.
+ */
 export interface StateStore {
   open(networkName: string): void
   close(): Promise<void>
